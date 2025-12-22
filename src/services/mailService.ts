42ba5b25a41a48987f7d24945cb71c657e8fc188
Rhyan Oliveira <Rhyanoliveirads@gmail.com>
2025-12-22 17:42:13 +0000

    refactor email message

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -3,10 +3,10 @@ import {mailProvider} from '../infra/mail.ts'
 export async function sendEmail(email:string,userName:string) {
 
   await mailProvider.sendMail({
-    from: '"Teste Backend" <rhyanlindorp@gmail.com>',
+    from: '"Mail sender test" <rhyanlindorp@gmail.com>',
     to: email,
     subject: 'Welcome Back!',
-    html: `<h1>Bem-vindo${userName ? `, ${userName}` : ''}!</h1><p>Bom te ver por aqui.</p>`,
+    html: `<h1>Welcome Back!${userName ? `, ${userName}` : ''}! Good to see you again.</p>`,
   })
 
 
