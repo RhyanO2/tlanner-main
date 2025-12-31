13043118ee5ea2d5b46748c023c560e1bc1d7fc2
Rhyan Oliveira <Rhyanoliveirads@gmail.com>
2025-12-30 23:57:48 -0300

    Delete dist directory

--- dist/routes/testRoute.js
+++
@@ -1,9 +0,0 @@
-export const TESTROUTE = async (server) => {
-    server.get('/', {
-        schema: {
-            summary: 'Check the connection with server'
-        }
-    }, async (req, res) => {
-        res.status(418).send({ WORKING: 'This is fine!👍' });
-    });
-};
