b4d569423f290fad3890f44961b252ee0185c2ee
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-02 10:35:54 -0300

    .

--- src/routes/taskCreate.ts
+++ src/routes/taskCreate.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import z from 'zod';
+
 import { checkRequestJWT } from './hooks/checkJWT-FromReq.js';
 import { postTask } from '../controllers/taskControllers.js';
 
@@ -16,10 +17,19 @@ export const createTask: FastifyPluginAsyncZod = async (server) => {
           // status: z.enum([]),
           due_date: z.string(),
           // workspaceID: z.string(),
+          priority: z.enum(['low', 'normal', 'high', 'urgent']),
         }),
         response: {
           201: z.object({
-            message: z.string(),
+            task: z.object({
+              title: z.string(),
+              description: z.string(),
+              due_date: z.date(),
+              priority: z.enum(['low', 'normal', 'high', 'urgent']),
+              id: z.uuid(),
+              status: z.enum(['pending', 'in_progress', 'done']),
+              id_workspace: z.uuid(),
+            }),
           }),
         },
       },
