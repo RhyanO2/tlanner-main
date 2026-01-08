45b76dc69f9bd11f92b5ed497a849122917cddba
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-08 08:07:53 -0300

    .

--- src/infra/mail.ts
+++ src/infra/mail.ts
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer';
 
 export const mailProvider = nodemailer.createTransport({
   host: 'smtp.resend.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: 'resend',
     pass: process.env.MAIL_KEY,
