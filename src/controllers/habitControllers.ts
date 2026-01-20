5b17bb49e50bfb147f40f289ef994f72b009121c
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-20 16:10:54 -0300

    HabitsControllers

---
+++ src/controllers/habitControllers.ts
@@ -0,0 +1,73 @@
+import {
+  habitsCreate,
+  habitEdit,
+  habitRemove,
+  habitsGet,
+} from '../services/habitService';
+import { type FastifyRequest, type FastifyReply } from 'fastify';
+
+export async function getHabitsByUserID(
+  req: FastifyRequest,
+  res: FastifyReply
+) {
+  try {
+    const { userID } = req.params as { userID: string };
+    const result = await habitsGet(userID);
+    res.status(200).send({
+      tasks: result,
+    });
+  } catch (err: any) {
+    res.status(err.statuscode || 400).send({
+      message: err.message,
+    });
+  }
+}
+
+export async function postHabit(req: FastifyRequest, res: FastifyReply) {
+  // const { userid } = req.params as {
+  //   userid: string;
+  // };
+  const { name, frequency, id_user } = req.body as {
+    name: string;
+    frequency: 'daily' | 'weekly' | 'monthly';
+    id_user: string;
+  };
+  try {
+    const habits = await habitsCreate(name, frequency, id_user);
+    res.status(201).send({ habits: habits });
+  } catch (err: any) {
+    res.status(err.statuscode || 400).send({
+      message: err.message,
+    });
+  }
+}
+
+export async function editHabit(req: FastifyRequest, res: FastifyReply) {
+  const { id } = req.params as { id: string };
+  const { name, frequency } = req.body as {
+    name: string;
+    frequency: 'daily' | 'weekly' | 'monthly';
+  };
+  try {
+    await habitEdit(name, frequency, id);
+    res.status(200).send({ message: 'Habit edited!' });
+  } catch (err: any) {
+    res.status(err.statuscode || 400).send({
+      message: err.message,
+    });
+  }
+}
+
+export async function delHabit(req: FastifyRequest, res: FastifyReply) {
+  try {
+    const { id } = req.params as { id: string };
+
+    await habitRemove(id);
+
+    res.status(200).send({ message: 'Habit deleted!' });
+  } catch (err: any) {
+    res.status(err.statuscode || 400).send({
+      message: err.message,
+    });
+  }
+}
