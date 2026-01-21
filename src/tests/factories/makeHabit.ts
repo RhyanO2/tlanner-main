4c3059c3f4e7c6408d780b5010499a0f40967d44
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-21 15:50:45 -0300

    Habits factory

---
+++ src/tests/factories/makeHabit.ts
@@ -0,0 +1,24 @@
+import { faker as f } from '@faker-js/faker';
+
+import { db } from '../../database/index.js';
+import { Habits } from '../../database/schema.js';
+import { makeUser } from './makeUser.js';
+
+export async function makeHabit(userID?: string) {
+  const { user } = await makeUser();
+
+  const Habit = await db
+    .insert(Habits)
+    .values({
+      name: f.lorem.words(2),
+      frequency: 'daily',
+      id_user: userID || user.id,
+    })
+    .returning();
+
+  return Habit[0];
+}
+
+// const taskid =  (makeTask()).id;
+
+// console.log(taskid);
