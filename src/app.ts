3e3836d0acc8c46e99ef6ab49f58e58b37d93a45
RhyanO2 <rhyanoliveirads@gmail.com>
2026-02-10 09:52:17 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,14 @@ import { HabitPOST } from './routes/HabitCreate';
 import { habitPUT } from './routes/HabitUpdate';
 import { habitDelete } from './routes/HabitDelete';
 import { habitsGET } from './routes/HabitView';
+import rateLimit from '@fastify/rate-limit'
+
+
+   server.register(rateLimit, {
+     max: 100,
+     timeWindow: '15 minutes'
+   })
+
 
 server.withTypeProvider<ZodTypeProvider>();
 
