1669d930d53a332f4db775c6388d2e9ef8f07224
RhyanO2 <rhyanoliveirads@gmail.com>
2026-02-12 16:54:12 -0300

    .

--- src/tests/helpers/db.helper.ts
+++ src/tests/helpers/db.helper.ts
@@ -1,10 +1,9 @@
 // src/tests/helpers/db.helper.ts
 import { db } from '../../database/index.js';
-import { Habits, Tasks, Users, Workspace } from '../../database/schema.js';
-
+import { sql } from 'drizzle-orm';
 export async function cleanTestDatabase() {
-  await db.delete(Tasks);
-  await db.delete(Habits);
-  await db.delete(Workspace);
-  await db.delete(Users);
+  await db.execute(sql`
+    TRUNCATE TABLE "Tasks", "Habits", "Workspace", "Users" 
+    RESTART IDENTITY CASCADE
+  `);
 }
