814492afeb119f2d09b8b5b23bea9c1b13e6a855
RhyanO2 <rhyanoliveirads@gmail.com>
2026-02-14 13:29:42 -0300

    cache applied operations

--- src/services/habitService.ts
+++ src/services/habitService.ts
@@ -1,3 +1,4 @@
+import { deleteCache, getCache, setCache } from '../cache/cacheUtils';
 import { AppError } from '../errors/AppError';
 import {
   getHabitsByID,
@@ -8,11 +9,19 @@ import {
 } from '../models/habitModel';
 
 export async function habitsGet(userID: string) {
+  const cacheKey = `habits:user:${userID}`;
+
+  const cached = await getCache<typeof habits>(cacheKey);
+
+  if (cached) return cached;
+
   const habits = await getHabitsByUserID(userID);
 
   if (habits.length === 0) {
     throw new AppError(`user: ${userID} don't have any habits`, 404);
   }
+
+  await setCache(cacheKey, habits, 500);
   return habits;
 }
 export async function habitsCreate(
@@ -21,6 +30,8 @@ export async function habitsCreate(
   userID: string
 ) {
   const createHabit = await HabitInsert(name, frequency, userID);
+
+  await deleteCache(`habits:user:${userID}`);
 
   return createHabit;
 }
@@ -38,6 +49,8 @@ export async function habitEdit(
 
   const editedHabit = await HabitUpdate(name, frequency, habitID);
 
+  await deleteCache(`habits:user:${habit[0].id_user}`);
+
   return editedHabit;
 }
 
@@ -48,6 +61,8 @@ export async function habitRemove(habitID: string) {
     throw new AppError('task cannot be find', 404);
   }
   const deletedHabit = await HabitDelete(habitID);
+
+  await deleteCache(`habits:user:${habit[0].id_user}`);
 
   return deletedHabit;
 }
--- src/services/workspaceService.ts
+++ src/services/workspaceService.ts
@@ -1,6 +1,10 @@
-
+import {
+  deleteCache,
+  deleteCacheByPattern,
+  getCache,
+  setCache,
+} from '../cache/cacheUtils.js';
 import { AppError } from '../errors/AppError.js';
-
 
 import {
   selectUserWorkspaces,
@@ -11,33 +15,49 @@ import {
 } from '../models/workspaceModel.js';
 
 export async function userWorkspacesGet(userID: string) {
+  const cacheKey = `workspaces:user:${userID}`;
+
+  const cached = await getCache<{ workspaces: typeof workspaces }>(cacheKey);
+
+  if (cached) return cached;
+
   const workspaces = await selectUserWorkspaces(userID);
 
   if (workspaces.length === 0) {
     throw new AppError('User do not have workspaces yet', 404);
   }
+  const result = { workspaces };
+  await setCache(cacheKey, workspaces, 300);
 
-  return { workspaces };
+  return result;
 }
 
 export async function WorkspaceGet(workspaceID: string) {
+  const cacheKey = `workspace:${workspaceID}`;
+
+  const cached = await getCache<typeof workspace>(cacheKey);
+  if (cached) return cached;
   const workspace = await selectWorkspaceById(workspaceID);
 
   if (workspace.length === 0) {
     throw new AppError(`${workspaceID} workspace cannot be founded`, 404);
   }
-
+  await setCache(cacheKey, workspace, 300);
   return workspace;
 }
 
 export async function WorkspaceCreate(title: string, userID: string) {
   const createWorkspace = await insertWorkspace(title, userID);
-
+  await deleteCache(`workspace:user${userID}`);
   return createWorkspace;
 }
 
 export async function WorkspaceEdit(title: string, workspaceID: string) {
   const editWorkspace = await updateWorkspaceTitle(title, workspaceID);
+
+  await deleteCache(`workspaces:${workspaceID}`);
+
+  await deleteCache(`workspaces:user:${editWorkspace.id_user}`);
 
   return editWorkspace;
 }
@@ -50,6 +70,14 @@ export async function WorkspaceDelete(workspaceID: string) {
   }
 
   const deleteWorkspace = await removeWorkspace(workspaceID);
+
+  await deleteCache(`workspace:${workspaceID}`);
+
+  // Remove o cache dos workspaces do usuário
+  await deleteCache(`workspaces:user:${workspace[0].id_user}`);
+
+  // Remove as tasks cacheadas desse workspace (cascade!)
+  await deleteCacheByPattern(`tasks:workspace:${workspaceID}`);
 
   return deleteWorkspace;
 }
