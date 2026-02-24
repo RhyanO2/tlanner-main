fd18a9c23e3f5194334738659e13714c02d63b36
RhyanO2 <rhyanoliveirads@gmail.com>
2026-02-24 20:20:54 -0300

    abacatepay-tests

---
+++ src/tests/abacatePayCheckQrcodePixStatus.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'vitest';
+import request from 'supertest';
+
+import { authenticateCreatedUser } from './factories/makeUser';
+import { server } from '../app';
+
+describe('abacatePayCreateQRcodePixtests', () => {
+  test('Check status QRcode Pix', async () => {
+    await server.ready();
+
+    const { token, user } = await authenticateCreatedUser();
+
+    const response = await request(server.server)
+      .get(`/pixQrCode/check?id=${'pix_char_0s10FAfCFqWdwaGQM46FTJ5U'}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('Check status QRcode Pix fail', async () => {
+    await server.ready();
+
+    const { token, user } = await authenticateCreatedUser();
+
+    const response = await request(server.server)
+      .get(`/pixQrCode/check?id=${''}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    expect(response.status).toEqual(400);
+  });
+});
---
+++ src/tests/abacatePayCreateQRcodePix.spec.ts
@@ -0,0 +1,58 @@
+import { describe, expect, test } from 'vitest';
+import request from 'supertest';
+
+import { authenticateCreatedUser } from './factories/makeUser';
+import { server } from '../app';
+
+describe('abacatePayCreateQRcodePixtests', () => {
+  test('Create QRcode Pix', async () => {
+    await server.ready();
+
+    const { token, user } = await authenticateCreatedUser();
+
+    const response = await request(server.server)
+      .post('/pixQrCode/create')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token)
+      .send({
+        amount: 100,
+        expiresIn: 123,
+        description: 'aa',
+        customer: {
+          name: 'Rhyan',
+          cellphone: '(88) 9302-8266',
+          email: 'rhyanoliveira@proton.me',
+          taxId: '631.172.133-33',
+        },
+        metadata: {
+          externalId: '123',
+        },
+      });
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('Create QRcode Pix Fail', async () => {
+    await server.ready();
+
+    const { token, user } = await authenticateCreatedUser();
+
+    const response = await request(server.server)
+      .post('/billing/create')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token)
+      .send({
+        amount: 100,
+        expiresIn: 123,
+        description: 'aa',
+        customer: {
+          name: 'Rhyan',
+        },
+        metadata: {
+          externalId: '123',
+        },
+      });
+
+    expect(response.status).toEqual(400);
+  });
+});
