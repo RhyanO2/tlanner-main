b562a3c39c689eb39db96cb4fa2f168ddcc5fb1b
RhyanO2 <rhyanoliveirads@gmail.com>
2026-03-01 22:16:22 -0300

    .

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,9 +2,6 @@ import Redis from 'ioredis';
 import 'dotenv/config';
 
 const url = process.env.REDIS_URL;
-// const token = process.env.REDIS_TOKEN;
-
-console.log('REDIS_URL:', url);
 
 if (!url) {
   console.warn('⚠️ Redis não configurado');
